20344f6cb9165bf7a7c0dc21ba41e2e79eef30d4
MarinTeskera <marin.teskera2002@gmail.com>
2021-07-27 19:42:58 +0200

    random commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:sum_game/question.dart';
 import 'package:sum_game/resetButton.dart';
 import 'package:sum_game/sumText.dart';
 import 'package:flutter/material.dart';
-//TODO: implement scrollable
+//TODO: implement scrollable change
 
 void main() {
   runApp(MyApp());
